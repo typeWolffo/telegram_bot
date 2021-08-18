14a6beed4c82b1c4a48a7c1bdff1e4ca0faab750
typeWolffo <jwilk436@gmail.com>
2021-08-18 09:23:41 +0200

    inline buttons callback func

--- index.js
+++ index.js
@@ -22,35 +22,23 @@ class Player {
 }
 
 
-
 bot.on('/register', (msg) => {
 
-    function setPlayerWithFigure(fig, win, lose) {
-        let figure = new Figure(fig, win, lose)
-        let player = new Player(msg.from.username, figure);
-        players.push(player);
-    }
 
     let replyMarkup = bot.inlineKeyboard([
         [
-            bot.inlineButton('Rock', {callback: setPlayerWithFigure('rock', 'scissors', 'paper')}),
-            bot.inlineButton('Paper', {callback: setPlayerWithFigure('paper', 'rock', 'scissors')}),
-            bot.inlineButton('Scissors', {callback: setPlayerWithFigure('scissors', 'paper', 'rock')}),
+            bot.inlineButton('Rock', {callback: 'figure_rock'}),
+            bot.inlineButton('Paper', {callback: 'figure_paper'}),
+            bot.inlineButton('Scissors', {callback: 'figure_scissors'}),
         ]
-    ]);
-console.log(players)
-    return bot.sendMessage(msg.chat.id, `${registerTimeleft}`).then(re => {
-        timer(msg.chat.id, re.message_id);
-        bot.sendMessage(msg.from.id, 'Choose', {replyMarkup});
-
-    });
+    ], {resize: true});
 
     function timer(chatId, messageId) {
         let countdown = setInterval(async () => {
             registerTimeleft -= 1;
             if (registerTimeleft <= 0) {
                 clearInterval(countdown);
-                bot.sendMessage(chatId, 'Rejestracja zamknięta');
+                bot.sendMessage(chatId, 'Register stop:.');
             }
             console.log(registerTimeleft);
             console.log(chatId);
@@ -61,23 +49,61 @@ console.log(players)
         }, 1000);
     }
 
+    bot.on('callbackQuery', msg => {
+        const action = msg.data;
+        let figure = new Figure();
+        console.log('callback: ', action)
+        switch (action) {
+            case 'figure_rock':
+                figure.name = 'rock';
+                figure.winsWith = 'scissors';
+                figure.losesTo = 'paper'
+                break;
+            case 'figure_paper':
+                figure.name = 'paper';
+                figure.winsWith = 'rock';
+                figure.losesTo = 'scissors'
+                break;
+            case 'figure_scissors':
+                figure.name = 'scissors';
+                figure.winsWith = 'paper';
+                figure.losesTo = 'rock'
+                break;
+        }
+        console.log(figure)
+
+        // function setPlayerWithFigure(fig, win, lose) {
+        //     let figure = new Figure(fig, win, lose)
+        //     let player = new Player(msg.from.username, figure);
+        //     players.push(player);
+        // }
+    });
+
+
+    return bot.sendMessage(msg.chat.id, `${registerTimeleft}`).then(re => {
+        timer(msg.chat.id, re.message_id);
+        bot.sendMessage(msg.from.id, 'Choose your figure    ', {replyMarkup});
+    });
 })
 
-bot.on('/prs', (msg) => {
 
-    function registerPlayers() {
-        players.push(msg.from.username);
-        console.log(players);
-        console.log(msg);
+// bot.on('/prs', (msg) => {
+//
+//     function registerPlayers() {
+//         players.push(msg.from.username);
+//         console.log(players);
+//         console.log(msg);
+//
+//     }
+//
+//     async function prsGame() {
+//         await registerPlayers();
+//     }
+//
+//     prsGame();
+// });
 
-    }
 
-    async function prsGame() {
-        await registerPlayers();
-    }
-
-    prsGame();
-});
 bot.on('/test', msg => {
 
     exec(`powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c cd / && echo test  > test.TEST'"`, (error, stdout, stderr) => {
