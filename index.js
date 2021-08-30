6eb3110b05eea41d7113214726ffc21ed9980951
typeWolffo <jwilk436@gmail.com>
2021-08-30 22:32:53 +0200

    nothing works as planned

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const TeleBot = require('telebot');
-const {exec} = require("child_process");
 const bot = new TeleBot(process.env.TELEGRAM_API_KEY);
 
 players = [];
@@ -23,7 +22,7 @@ class Player {
 }
 
 
-bot.on('/register', (msg) => {
+bot.on('/r', (msg) => {
 
     function timer(chatId, messageId) {
         let countdown = setInterval(async () => {
@@ -33,24 +32,36 @@ bot.on('/register', (msg) => {
                 bot.sendMessage(chatId, 'Register stop:.');
             }
             console.log(registerTimeleft);
-            console.log(chatId);
 
-            bot.editMessageText({chatId, messageId}, `${registerTimeleft}`, {parseMode: 'html'}
-            ).catch(error => console.log('Error:', error));
+            bot.editMessageText({
+                chatId,
+                messageId
+            }, `${registerTimeleft}`, {
+                parseMode: 'html'
+            }).catch(error => console.log('Error:', error));
 
         }, 1000);
     }
 
     let replyMarkup = bot.inlineKeyboard([
         [
-            bot.inlineButton('Rock', {callback: 'figure_rock'}),
-            bot.inlineButton('Paper', {callback: 'figure_paper'}),
-            bot.inlineButton('Scissors', {callback: 'figure_scissors'}),
+            bot.inlineButton('Rock', {
+                callback: 'figure_rock'
+            }),
+            bot.inlineButton('Paper', {
+                callback: 'figure_paper'
+            }),
+            bot.inlineButton('Scissors', {
+                callback: 'figure_scissors'
+            }),
         ]
-    ], {resize: true});
+    ], {
+        resize: true
+    });
 
     bot.on('callbackQuery', msg => {
         const action = msg.data;
+
         let figure = new Figure();
         switch (action) {
             case 'figure_rock':
@@ -71,18 +82,71 @@ bot.on('/register', (msg) => {
         }
 
         function addNewPlayer() {
-            let player = new Player(msg.from.username, figure.name);
+            let player = {
+                playerName: msg.from.username,
+                chosenFigure: figure.name
+            }
             players.push(player);
         }
 
         function makePlayersUnique() {
-            playersFiltered = uniquePlayers(players, player => player.name);
+            playersFiltered = uniquePlayers(players, player => player.playerName);
         }
 
         function determineWinner() {
-            for (let player of playersFiltered) {
-                console.log(player)
+
+            while (playersFiltered.length > 0) {
+                const setWinner = () => {
+
+                const firstPlayer = playersFiltered[0];
+
+                const getSecondRandomPlayer = () => {
+                    for (let players in playersFiltered) {
+                        let randomPlayerIndex = Math.floor(Math.random() * (players - 1 + 1)) + 1;
+                        return randomPlayerIndex;
+                    }
+                }
+
+                let secondPlayer = playersFiltered[getSecondRandomPlayer()];
+                    if (secondPlayer) {
+                        if (firstPlayer.chosenFigure === secondPlayer.chosenFigure) {
+                            // setWinner();
+                        } else if (firstPlayer.chosenFigure == 'rock') {
+                            if (secondPlayer.chosenFigure == 'paper') {
+                                playersFiltered.splice(0, 1); // remove first player from playersFiltered array
+                                console.log('stage 1 :: first removed')
+                                // setWinner();
+                            } else {
+                                playersFiltered.splice(getSecondRandomPlayer(), 1); // remove chosen randomly second player from playersFiltered array
+                                console.log('stage 2 :: second removed')
+                                // setWinner();
+                            }
+                        } else if (firstPlayer.chosenFigure == 'scissors') {
+                            if (secondPlayer.chosenFigure == 'rock') {
+                                playersFiltered.splice(0, 1);
+                                console.log('stage 2 :: first removed')
+                                // setWinner();
+                            } else {
+                                playersFiltered.splice(getSecondRandomPlayer(), 1);
+                                console.log('stage 2 :: second removed')
+                                // setWinner();
+                            }
+                        } else if (firstPlayer.chosenFigure == 'paper') {
+                            if (secondPlayer.chosenFigure == 'scissors') {
+                                playersFiltered.splice(0, 1);
+                                console.log('stage 3 :: first removed')
+                                // setWinner();
+                            } else {
+                                playersFiltered.splice(getSecondRandomPlayer(), 1);
+                                console.log('stage 3 :: second removed')
+                                // setWinner();
+                            }
+                        }
+                        console.log('Winner :: ', playersFiltered[0]);
+                    }
+                }
             }
+            console.log('winner')
         }
 
         (async function gameHandler() {
@@ -100,28 +164,12 @@ bot.on('/register', (msg) => {
         ]
     }
 
-
     return bot.sendMessage(msg.chat.id, `${registerTimeleft}`).then(re => {
         timer(msg.chat.id, re.message_id);
-        bot.sendMessage(msg.chat.id, 'Choose your figure    ', {replyMarkup});
+        bot.sendMessage(msg.chat.id, 'Choose your figure    ', {
+            replyMarkup
+        });
     });
 })
-
-
-bot.on('/test', msg => {
-
-    exec(`powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c cd / && echo test  > test.TEST'"`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
-})
-
 
 bot.start();
